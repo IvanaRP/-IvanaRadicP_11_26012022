772275b2f7ee82446e21f7e3e8957733360ab189
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2022-02-02 15:04:28 +0100

    location
    location info onClick event

--- kasa/src/components/Description/index.jsx
+++ kasa/src/components/Description/index.jsx
@@ -1,7 +1,13 @@
 // import {handleClickShow} from '../utils/function/event'
-import PropTypes from 'prop-types'
-
+import PropTypes from "prop-types";
+import React from "react";
 import "../../styles/Description.css";
+
+
+import { FaChevronDown } from 'react-icons/fa';
+
+
+
 
 /**
  * Description component
@@ -10,21 +16,35 @@ import "../../styles/Description.css";
  * using handleClickShow function for displaying or not the content. By default content is not displayed.
  */
 
-function Description({description}) {   
+function Description({ description }) {
 
-    return (
-        <div className="descrWrapper">
-            <div className="descr-header">
-                <span>Description</span>
-                <span id="chevron-descr" className="chevron top" ></span>
-            </div>
-            <div id="descrContent" className="descr-content">{description}</div>
-        </div>
-    )
+  function handleClick(e) {
+    const content = document.getElementById("descrContent");
+
+    if (content.style.display === "none") {
+      content.style.display = "flex";
+    } else {
+      content.style.display = "none";
+    }
+  }
+
+  return (
+    <div className="descrWrapper">
+      <div className="descr-header" >
+        <span>Description </span>
+        <span id="chevron-descr" >
+        <FaChevronDown className="chevron-icon-down" onClick={handleClick}/>
+        </span>
+      </div>
+      <div id="descrContent" className="descr-content">
+        {description}
+      </div>
+    </div>
+  );
 }
 
 Description.propTypes = {
-    description: PropTypes.string.isRequired
-}
+  description: PropTypes.string.isRequired,
+};
 
-export default Description
+export default Description;
--- kasa/src/components/Equipment/index.jsx
+++ kasa/src/components/Equipment/index.jsx
@@ -1,36 +1,48 @@
 // import {handleClickShow} from '../utils/function/event'
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import "../../styles/Equipment.css";
 
+import { FaChevronDown } from "react-icons/fa";
+
 /**
- * 
- * @param {props} equipment  
+ *
+ * @param {props} equipment
  * @returns the location equipements
  * using handleClickShow function for displaying or not the content. By default content is not displayed.
  */
 
-function Equipment({equipment}) {
-    return (
-        <div className="equipWrapper">
-            <div className="equip-header">
-                <span>Equipements</span>
-                <span id="chevron-equip" className="chevron top" ></span>
-            </div>
-            <div className="equip-content" id="equipContent">
-                <ul>
-                    {equipment.map((eq, index) => 
-                        <li key={index}>
-                            {eq}
-                        </li>
-                    )}
-                </ul>                
-            </div>
-        </div>
-    )
+function Equipment({ equipment }) {
+  function handleClick(e) {
+    const content = document.getElementById("equipContent");
+
+    if (content.style.display === "none") {
+      content.style.display = "flex";
+    } else {
+      content.style.display = "none";
+    }
+  }
+
+  return (
+    <div className="equipWrapper">
+      <div className="equip-header">
+        <span>Equipements</span>
+        <span id="chevron-descr">
+          <FaChevronDown className="chevron-icon-down" onClick={handleClick} />
+        </span>
+      </div>
+      <div className="equip-content" id="equipContent">
+        <ul>
+          {equipment.map((eq, index) => (
+            <li key={index}>{eq}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 Equipment.propTypes = {
-    equipment: PropTypes.array.isRequired
-}
+  equipment: PropTypes.array.isRequired,
+};
 
-export default Equipment
+export default Equipment;
