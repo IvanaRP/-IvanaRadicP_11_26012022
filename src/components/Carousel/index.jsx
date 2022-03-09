882a508984a74a4d2a472f0c66cc059c7e1dbf42
IvanaRP <83823351+IvanaRP@users.noreply.github.com>
2022-03-09 17:03:03 +0100

    carousel
    added number

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,95 +1,57 @@
-import { useRef, useEffect } from "react";
+import { useState } from 'react'
 import PropTypes from "prop-types";
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import '../../styles/Carousel.css'
 
 
+
+
 function Carousel({ cover, pictures }) {
 
 
+    const [current, setCurrent] = useState(0)
+    const counter = pictures.length
 
-  const currentPicture = useRef(null);
-  const counter = pictures.length;
- 
-  
-
-  let pictureCounter = 0;
-  useEffect(() => startCarousel());
-
-  const startCarousel = () => {
-    currentPicture.current.src = pictures[0];
-  };
-
-  const handleCarousel = (picture) => {
-    currentPicture.current.src = pictures[picture - 1];
-    animateCarousel(currentPicture);
-  };
-
-  const animateCarousel = () => {
-    currentPicture.current.classList.add("fadeIn");
-    setTimeout(() => {
-      currentPicture.current.classList.remove("fadeIn");
-    }, 700);
-  };
-
-  const goToPreviousPicture = () => {
-    if (pictureCounter === 0) {
-      handleCarousel(pictures.length);
-      pictureCounter = pictures.length;
+    const goToPreviousPicture = () => {
+        setCurrent(current === counter - 1 ? 0 : current + 1)
+    }
+    const gotToNextPicture = () => {
+        setCurrent(current === 0 ? counter - 1 : current - 1)
     }
 
-    handleCarousel(pictureCounter);
-    pictureCounter--;
-  };
-
-  const gotToNextPicture = () => {
-    if (pictureCounter === pictures.length - 1) {
-      startCarousel();
-      pictureCounter = -1;
-    }
-
-    currentPicture.current.src = pictures[pictureCounter + 1];
-    pictureCounter++;
-  };
-
-
-  console.log([currentPicture.length] + 1);
-  console.log(pictureCounter + 1);
-  console.log(counter);
-  console.log(pictures[pictureCounter + 1])
-  console.log([currentPicture].length)
-  console.log(currentPicture.current.src);
-
-  return (
-    <div className="slideWrapper">
-   
-      <div className="slideContainer">
-        {counter === 1 ? (
-          <img src={cover} alt="Carrousel"  />
-        ) : (
-          <div>
-             <FaChevronLeft id="chevron-icon-down" className="btn-left" onClick={goToPreviousPicture}/>
-            <img
-              src={currentPicture}
-              ref={currentPicture}
-              alt=""
-              className="slideImg"
-            />
-    <p> {pictureCounter + 1}/{counter}</p>
-             <FaChevronRight id="chevron-icon-down" className="btn-right" onClick={gotToNextPicture}/>
+    
+    return (
+        <div className="slideWrapper">
+       
+          <div className="slideContainer">
+            {counter === 1 ? (
+              <img src={cover} alt="Carrousel"  />
+            ) : (
+              <div>
+                 <FaChevronLeft id="chevron-icon-down" className="btn-left" onClick={goToPreviousPicture}/>
+                 {pictures.map(
+                    (pic, index) =>
+                        index === current && (
+                            <img  key={pic} src={pic} alt="" className="slideImg" />
+                        )
+                  )}
+                  <p className="slideNumber"> {current + 1}/{counter}</p>
+                 <FaChevronRight id="chevron-icon-down" className="btn-right" onClick={gotToNextPicture}/>
+              </div>
+              
+            )}
+            
           </div>
-          
-        )}
-         <p> {currentPicture + 1}/{counter}</p>
-      </div>
-    </div>
-  );
+        </div>
+      );
+
+
 }
 
-Carousel.propTypes = {
-  id: PropTypes.string.isRequired,
-  cover: PropTypes.string.isRequired,
-  pictures: PropTypes.array.isRequired,
-};
 
-export default Carousel;
+Carousel.propTypes = {
+    id: PropTypes.string.isRequired,
+    cover: PropTypes.string.isRequired,
+    pictures: PropTypes.array.isRequired,
+  };
+export default Carousel
